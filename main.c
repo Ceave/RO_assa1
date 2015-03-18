5ccabc207357b77294cd20b0341774d485bf48ba
Stefan Maier <Stefan@tug-swl-g191.tu-graz.ac.at>
2015-03-18 16:26:54 +0100

    engelschoere singen

--- main.c
+++ main.c
@@ -18,23 +18,31 @@
 #include <stdlib.h>
 
 int main(void){
-        uint16_t OutHigh = 0;
-        uint16_t OutLow = 0;
-        uint16_t temp = 0;
-        uint16_t MaxCount = 16;
-        uint16_t CurrentCount = MaxCount;
-        uint16_t ONE = 1;
-        uint16_t truthMask = 0;
-        uint16_t tempShiftLOW = 0;
-        uint16_t tempShiftHIGH = 0;
+        int16_t OutHigh = 0;
+        int16_t OutLow = 0;
+        int16_t temp = 0;
+        int16_t MaxCount = 16;
+        int16_t CurrentCount = MaxCount;
+        int16_t ONE = 1;
+        int16_t truthMask = 0;
+        int16_t tempShiftLOW = 0;
+        int16_t tempShiftHIGH = 0;
+        int16_t carryMask = 0;
+        int16_t addWork1 = 0;
+        int16_t addWork2 = 0;
+        int16_t addWorkResult = 0;
+        int16_t invertMask = 0xFFFF;
+        int16_t carry = 0;
         ///////////UNUSED IN ASSEMBLER/////////
-        uint16_t Input1;
-        uint16_t Input2;
-        uint16_t tempInput1;
-        uint16_t tempInput2;
+        int16_t Input1;
+        int16_t Input2;
+        int16_t tempInput1;
+        int16_t tempInput2;
         ///////////////////////////////////////
 START:  CurrentCount = 16;
-        printf("Result = %x, Outhigh = %x Outlow = %x ",Input1*Input2,OutHigh,OutLow);
+        OutHigh += carry;
+        printf("Result = %x \nOuthigh= %x \nOutlow =     %x",(uint16_t)Input1*(uint16_t)Input2,OutHigh,OutLow);
+        carry = 0;
         OutHigh = 0;
         OutLow = 0;
         printf("\nPlease enter two Numbers: \n");
@@ -42,7 +50,7 @@ START:  CurrentCount = 16;
         if(Input1 == 0){goto EXIT;}
         scanf("%hd",&Input2);
         if(Input2 == 0){goto EXIT;}
-        printf("\nAm i drunk? Input1: %d, Input2: %d \n",Input1,Input2);
+        //printf("\nAm i drunk? Input1: %d, Input2: %d \n",Input1,Input2);
 
 
 LOOP:   tempInput1 = Input1;
@@ -51,18 +59,45 @@ LOOP:   tempInput1 = Input1;
         tempInput2 = tempInput2 >> CurrentCount;
         truthMask = tempInput2 & ONE;
         if(truthMask == 0){goto SKIP;}
-        printf("Doing Math!\n");
+        //printf("Doing Math!\n");
         tempShiftLOW = Input1 << CurrentCount;
         temp = MaxCount - CurrentCount;
         tempShiftHIGH = Input1 >> temp;
         OutHigh = OutHigh + tempShiftHIGH;
+        addWork1 = OutLow;
+        addWork2 = tempShiftLOW;
         OutLow = OutLow + tempShiftLOW;
-
+        goto ADDC;//jump and link
+R1:
 SKIP:   if(CurrentCount == 0){goto START;}
         if (0 == 0){goto LOOP;}
     
 EXIT:   return 0;
-    
+  
+ADDC:   addWorkResult = addWork1 + addWork2;
+        carryMask = addWork1 & addWork2;
+        carryMask = carryMask ^ invertMask;
+        if(carryMask > 0){
+          goto CARRY;
+        }
+        if(carryMask == 0){
+          goto CARRY;
+        }
+        carryMask = addWork1 ^ addWork2;
+        addWorkResult = addWorkResult ^ invertMask;
+        carryMask = carryMask & addWorkResult;
+        carryMask = carryMask ^ invertMask;
+        if(carryMask > 0){
+          goto CARRY;
+        }
+        if(carryMask == 0){
+          goto CARRY;
+        }
+        goto R1;
+        //BZ und BP
+CARRY:  carry = carry + ONE;
+        goto R1; //this needs to be a jump and link
+
 }
 /*
  
