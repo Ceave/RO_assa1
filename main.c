473bc11ed494b55310f8c3efda5d78058d035b4a
Stefan Maier <Stefan@Stefans-MBP.fritz.box>
2015-03-19 15:39:31 +0100

    finally it works

--- main.c
+++ main.c
@@ -2,82 +2,75 @@
 #include <stdlib.h>
 
 int main(void){
-        int16_t OutHigh = 0;
-        int16_t OutLow = 0;
-        int16_t temp = 0;
-        int16_t MaxCount = 16;
-        int16_t CurrentCount = MaxCount;
-        int16_t ONE = 1;
-        int16_t truthMask = 0;
-        int16_t tempShiftLOW = 0;
-        int16_t tempShiftHIGH = 0;
-        int16_t carryMask = 0;
-        int16_t addWork1 = 0;
-        int16_t addWork2 = 0;
-        int16_t addWorkResult = 0;
-        int16_t invertMask = 0xFFFF;
-        int16_t carry = 0;
-        int16_t Input1;
-        int16_t Input2;
-        int16_t tempInput1;
-        int16_t tempInput2;
+  int OutHigh = 0; //R1
+  int OutLow = 0; //R2
+  int Mask = 0; //R3
+  int MaxCount = 32; //R4
+  int CurrentCount = MaxCount; //R5
+  int ONE = 1; //R6
+  int tempShiftLOW = 0; //R7
+  int tempShiftHIGH = 0; //R8
+  int addWork1 = 0; //R9
+  int addWork2 = 0; //RA
+  int tempInput2 = 0; //RB
+  int invertMask = 0xFFFFFFFF; //RC
+  int carry = 0; //RD
+  int Input1; //RE
+  int Input2; //RF
   
-        goto FIRSTIN;
+  goto FIRSTIN;
   
 START:  OutHigh += carry;
-        printf("Result = %x \nOuthigh= %x \nOutlow =     %x",(uint16_t)Input1*(uint16_t)Input2,OutHigh,OutLow);
-FIRSTIN:CurrentCount = 16;
-        carry = 0;
-        OutHigh = 0;
-        OutLow = 0;
-        printf("\n------------------------------\nPlease enter two Numbers: \n");
-        scanf("%hd",&Input1);
-        if(Input1 == 0){goto EXIT;}
-        scanf("%hd",&Input2);
-        if(Input2 == 0){goto EXIT;}
-
-
-LOOP:   tempInput1 = Input1;
-        tempInput2 = Input2;
+  printf("Result = %x \nOuthigh= %x \nOutlow =     %x",Input1*Input2,OutHigh,OutLow);
+FIRSTIN:
+  CurrentCount = 32;
+  carry = 0;
+  OutHigh = 0;
+  OutLow = 0;
+  printf("\n------------------------------\nPlease enter two Numbers: \n");
+  scanf("%d",&Input1);
+  if(Input1 == 0){goto EXIT;}
+  scanf("%d",&Input2);
+  if(Input2 == 0){goto EXIT;}
+  
+  
+LOOP:   tempInput2 = Input2;
         CurrentCount = CurrentCount - ONE;
         tempInput2 = tempInput2 >> CurrentCount;
-        truthMask = tempInput2 & ONE;
-        if(truthMask == 0){goto SKIP;}
+        Mask = tempInput2 & ONE;
+        if(Mask == 0){goto SKIP;}
         tempShiftLOW = Input1 << CurrentCount;
-        temp = MaxCount - CurrentCount;
-        tempShiftHIGH = Input1 >> temp;
+        Mask = MaxCount - CurrentCount;
+        if(CurrentCount == 0){goto SKIP2;}
+        tempShiftHIGH = Input1 >> Mask;
+        Mask = invertMask + 0;
+        Mask = Mask << CurrentCount;
+        Mask = Mask ^ invertMask;
+        tempShiftHIGH = tempShiftHIGH & Mask;
         OutHigh = OutHigh + tempShiftHIGH;
-        addWork1 = OutLow;
+SKIP2:  addWork1 = OutLow;
         addWork2 = tempShiftLOW;
         OutLow = OutLow + tempShiftLOW;
         goto ADDC;
 SKIP:   if(CurrentCount == 0){goto START;}
         if (0 == 0){goto LOOP;}
-    
+  
 EXIT:   return 0;
   
-ADDC:   addWorkResult = addWork1 + addWork2;
-        carryMask = addWork1 & addWork2;
-        carryMask = carryMask ^ invertMask;
-        if(carryMask > 0){
-          goto CARRY;
-        }
-        if(carryMask == 0){
-          goto CARRY;
-        }
-        carryMask = addWork1 ^ addWork2;
-        addWorkResult = addWorkResult ^ invertMask;
-        carryMask = carryMask & addWorkResult;
-        carryMask = carryMask ^ invertMask;
-        if(carryMask > 0){
-          goto CARRY;
-        }
-        if(carryMask == 0){
-          goto CARRY;
-        }
-        goto SKIP;
-
+ADDC:   tempInput2 = addWork1 + addWork2;
+  Mask = addWork1 & addWork2;
+  Mask = Mask ^ invertMask;
+  if(Mask > 0){goto CARRY;}
+  if(Mask == 0){goto CARRY;}
+  Mask = addWork1 ^ addWork2;
+  tempInput2 = tempInput2 ^ invertMask;
+  Mask = Mask & tempInput2;
+  Mask = Mask ^ invertMask;
+  if(Mask > 0){goto CARRY;}
+  if(Mask == 0){goto CARRY;}
+  goto SKIP;
+  
 CARRY:  carry = carry + ONE;
-        goto SKIP;
-
+  goto SKIP;
+  
 }
